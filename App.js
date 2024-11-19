376394179bc7e84415ca271ae7e83983af684d97
Lavisha Gaur <lavi9f2003@gmail.com>
2024-11-20 01:08:30 +0530

    images added

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ const resData = [
     duration: 35,
     cuisines: ["Pizzas"],
     address: "Vijay Nagar",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/38dd2056-8729-4a55-84ad-d927e6891edd_67385.jpg",
   },
   {
     resId: 2,
@@ -25,6 +26,7 @@ const resData = [
     duration: 45,
     cuisines: ["Chinese", "Asian"],
     address: "Treasure Island Indore",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/7/2b1d78bb-5604-46db-99ba-02de93dc58a2_881203.jpg"
   },
   {
     resId: 3,
@@ -33,6 +35,7 @@ const resData = [
     duration: 15,
     cuisines: ["Burgers", "Fast Food"],
     address: "Vijay Nagar",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/f2a0019c-c1f3-4fa8-8b53-b655a4e60da8_508468.JPG"
   },
   {
     resId: 4,
@@ -41,6 +44,7 @@ const resData = [
     duration: 35,
     cuisines: ["Waffle", "Desserts"],
     address: "BHAWANI COMPLEX",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5116a385bac0548e06c33c08350fbf11"
   },
   {
     resId: 5,
@@ -49,6 +53,7 @@ const resData = [
     duration: 25,
     cuisines: ["Burgers", "American"],
     address: "Malhar Mall",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/a5cee281-c25f-494a-8cab-ba7f49acfb03_806682.JPG"
   },
   {
     resId: 6,
@@ -57,6 +62,7 @@ const resData = [
     duration: 25,
     cuisines: ["Pizzas", "Italian"],
     address: "Meghdoot Park Road",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/24/dbc3b8be-18c9-44fc-93c9-a586fb0c6c47_65329.JPG"
   },
   {
     resId: 7,
@@ -65,6 +71,7 @@ const resData = [
     duration: 35,
     cuisines: ["Ice Cream", "Desserts"],
     address: "Vaibhav Nagar Sector A",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/9bc9e6f0-e139-4ee0-add7-9746bcc23c65_314060.JPG"
   },
   {
     resId: 8,
@@ -73,6 +80,7 @@ const resData = [
     duration: 35,
     cuisines: ["North Indian", "Thalis"],
     address: "South Tukoganj",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/g5txnz35wlrgbskk3r8y"
   },
   {
     resId: 9,
@@ -81,6 +89,7 @@ const resData = [
     duration: 25,
     cuisines: ["Indian", "Chinese"],
     address: "Vijay Nagar",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/lzm4nx5axlknsr914rvv"
   },
   {
     resId: 10,
@@ -89,6 +98,7 @@ const resData = [
     duration: 20,
     cuisines: ["Thalis", "South Indian"],
     address: "Bypass South",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cb1h7bmdd9hsbxvlix8f"
   },
   {
     resId: 11,
@@ -97,6 +107,7 @@ const resData = [
     duration: 25,
     cuisines: ["Chinese", "Thalis"],
     address: "Barfani Dham",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ubqbvnyhjnyrqqckrb2b"
   },
   {
     resId: 12,
@@ -105,6 +116,7 @@ const resData = [
     duration: 25,
     cuisines: ["North Indian", "Biryani"],
     address: "Vijay Nagar",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2acf0681a049dea7dac7271cf7ccc560"
   },
   {
     resId: 13,
@@ -113,6 +125,7 @@ const resData = [
     duration: 40,
     cuisines: ["Mughlai", "Biryani"],
     address: "Old Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x7zvrb5mbg92skzncwnu"
   },
   {
     resId: 14,
@@ -121,6 +134,7 @@ const resData = [
     duration: 40,
     cuisines: ["North Indian", "Mughlai"],
     address: "Bhawar Kuan",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ntfzr5mgmt9b9f545vtl"
   },
   {
     resId: 15,
@@ -129,6 +143,7 @@ const resData = [
     duration: 35,
     cuisines: ["Mughlai", "Biryani"],
     address: "Khajrana",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/pmpoofo6nnvnjvqzsbjd"
   },
   {
     resId: 16,
@@ -137,6 +152,7 @@ const resData = [
     duration: 30,
     cuisines: ["South Indian", "Chinese"],
     address: "South Tukoganj",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ouqegserv9k0zecemddl"
   },
   {
     resId: 17,
@@ -145,6 +161,7 @@ const resData = [
     duration: 25,
     cuisines: ["Chinese", "Desserts"],
     address: "Vijay Nagar",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/dgxgkfyf3tvpdpkg871z"
   },
   {
     resId: 18,
@@ -153,6 +170,7 @@ const resData = [
     duration: 30,
     cuisines: ["Snacks", "Sweets"],
     address: "New Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/obd9uwesga5zg6jpz66d"
   },
   {
     resId: 19,
@@ -161,6 +179,7 @@ const resData = [
     duration: 30,
     cuisines: ["Mughlai"],
     address: "classNameic Purnima Estate",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uya2owigz45a3o4kcta2"
   },
   {
     resId: 20,
@@ -169,6 +188,7 @@ const resData = [
     duration: 30,
     cuisines: ["Fast Food"],
     address: "New Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/n4inopqkfmhwkm436jij"
   },
   {
     resId: 21,
@@ -177,6 +197,7 @@ const resData = [
     duration: 30,
     cuisines: ["North Indian", "Thalis"],
     address: "Old Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a5a98c016e58866ddcd0fd2d1786d696"
   },
   {
     resId: 22,
@@ -185,6 +206,7 @@ const resData = [
     duration: 20,
     cuisines: ["Pizzas", "South Indian"],
     address: "Pardesipura",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/r3hc8fyoajjgkmjszx2k"
   },
   {
     resId: 23,
@@ -193,6 +215,7 @@ const resData = [
     duration: 30,
     cuisines: ["Thalis", "Rajasthani"],
     address: "New Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/vc195wpsfwlis8cxk0lx"
   },
   {
     resId: 24,
@@ -201,6 +224,7 @@ const resData = [
     duration: 20,
     cuisines: ["North Indian", "Chinese"],
     address: "Malwa Mill",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/nxc6w5pfmeinhicnqb58"
   },
   {
     resId: 25,
@@ -209,20 +233,20 @@ const resData = [
     duration: 25,
     cuisines: ["Chinese", "Fast Food"],
     address: "New Palasia",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iqxxrnl22lasvumrgcjl"
   },
 ];
 
-
 const FoodCard = (props) => {
   // Destructuring :
-  const {resId, resName, rating, duration, cuisines, address} = props.resData;
+  const { resId, resName, rating, duration, cuisines, address, img } = props.resData;
   return (
     <div className="Outer">
       <div className="Inner">
         <div className="FoodImg">
           <img
             className="FoodItem"
-            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/7/2b1d78bb-5604-46db-99ba-02de93dc58a2_881203.jpg"
+            src={props.resData.img}
             alt="Food Image"
           />
         </div>
